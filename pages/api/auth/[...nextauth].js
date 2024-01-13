8446cfeca080c017b45f5e9310e5ff43aa430c5b
OsmanMuratTurgut <86958091+OsmanMuratTurgut@users.noreply.github.com>
2024-01-13 16:39:23 +0300

    github-login

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../../../util/mongo";
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+});
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 import Input from "../../components/form/Input";
 import Title from "../../components/ui/Title";
 import { loginSchema } from "../../schema/login";
+import { useSession, signIn } from "next-auth/react";
 
 const Login = () => {
+  const { data: session } = useSession();
+  console.log(session);
   const onSubmit = async (values, actions) => {
     await new Promise((resolve) => setTimeout(resolve, 4000));
     actions.resetForm();
@@ -46,7 +49,7 @@ const Login = () => {
         className="flex flex-col items-center my-20 md:w-1/2 w-full mx-auto"
         onSubmit={handleSubmit}
       >
-        <Title addClass="text-[40px] mb-6">Login</Title>
+        <Title addClass="text-[40px] mb-6">Giriş</Title>
         <div className="flex flex-col gap-y-3 w-full">
           {inputs.map((input) => (
             <Input
@@ -58,14 +61,20 @@ const Login = () => {
           ))}
         </div>
         <div className="flex flex-col w-full gap-y-3 mt-6">
-          <button className="btn-primary">LOGIN</button>
-          <button className="btn-primary !bg-secondary">
+          <button className="btn-primary" type="submit">
+            Giriş
+          </button>
+          <button
+            className="btn-primary !bg-secondary"
+            type="button"
+            onClick={() => signIn("github")}
+          >
             <i className="fa fa-github mr-2 text-lg"></i>
             GITHUB
           </button>
           <Link href="/auth/register">
             <span className="text-sm underline cursor-pointer text-secondary">
-              Do you no have a account?
+              Şifremi unuttum
             </span>
           </Link>
         </div>
